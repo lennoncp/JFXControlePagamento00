f3ec3397ad0dcee8bc2dd69cd06c6c9ebed88c7f
Lennon <lennon.palmerio@gmail.com>
2019-11-06 00:02:49 -0300

    Designer Table View Parcelas

--- src/app/ParcelasController.java
+++ src/app/ParcelasController.java
@@ -1,14 +1,117 @@
 package app;
 
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleLongProperty;
-import javafx.beans.property.SimpleStringProperty;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.ResourceBundle;
 
-public class ParcelasController {
+import app.dao.AcordoDAO;
+import app.dao.ClienteDAO;
+import app.dao.ParcelaDAO;
+import app.model.Acordo;
+import app.model.Cliente;
+import app.model.Parcela;
+import app.model.TableViewParcela;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class ParcelasController implements Initializable{
 	
+	private ParcelaDAO pdao = new ParcelaDAO();
+	private ClienteDAO cdao = new ClienteDAO();
+	private AcordoDAO adao = new AcordoDAO();
 	
+	private ObservableList<TableViewParcela> parcelas = FXCollections.observableArrayList();
 	
-	
-	
+	Calendar calendar = Calendar.getInstance();
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    @FXML
+    private TextField txfNome;
+
+    @FXML
+    private TextField txfCliente;
+
+    @FXML
+    private TextField txfAcordo;
+
+    @FXML
+    private TextField txfParcela;
+
+    @FXML
+    private TextField txfValor;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private TableView<Parcela> tvParcelas;
+
+    @FXML
+    private TableColumn<Parcela, String> tcNome;
+
+    @FXML
+    private TableColumn<Parcela, String> tcAcordo;
+
+    @FXML
+    private TableColumn<Parcela, Integer> tcParcela;
+
+    @FXML
+    private TableColumn<Parcela, Double> tcValor;
+
+    @FXML
+    private TableColumn<Parcela, String> tcVencimento;
+
+    @FXML
+    private TableColumn<Parcela, String> tcpagamentos;
+
+    @FXML
+    private TableColumn<Parcela, ObservableList<Button>> tcButoes;
+
+    @FXML
+    void salvarParcela() {
+    	
+    	//TableViewParcela tvp = new TableViewParcela(codigo, cliente, acordo, parcela, valor, dataparcela, datapagamento)
+
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		tcNome.setCellValueFactory(new PropertyValueFactory("cliente"));
+		tcAcordo.setCellValueFactory(new PropertyValueFactory("acordo"));
+		tcParcela.setCellValueFactory(new PropertyValueFactory("parcela"));
+		tcVencimento.setCellValueFactory(new PropertyValueFactory("dataparcela"));
+		tcpagamentos.setCellValueFactory(new PropertyValueFactory("datapagamento"));
+		tcButoes.setCellValueFactory(new PropertyValueFactory("buttons"));
+		
+		ObservableList<Parcela> parcelas = FXCollections.observableArrayList();
+		
+		parcelas = pdao.getParcelas();
+		
+		for(Parcela p: parcelas) {
+			/*calendar.setTime(p.getDataParcela());
+			String vencimento = sdf.format(calendar.getTime());
+			calendar.setTime(p.getDataPagamento());
+			String pagamento = sdf.format(calendar.getTime());*/
+			
+			Cliente cliente = cdao.getCliente(p.getCliente());
+			
+			Acordo acordo = adao.getAcordo(p.getAcordo());
+			
+			TableViewParcela tvp = new TableViewParcela(p.getCodigo(), cliente, acordo, p.getParcela(), p.getValor(), p.getDataParcela(), p.getDataPagamento());
+			
+			tvParcelas.setItems(parcelas);
+		}
+		
+		
+	}
+
 }
--- src/app/model/TableViewParcela.java
+++ src/app/model/TableViewParcela.java
@@ -4,11 +4,18 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 public class TableViewParcela {
 	
@@ -20,6 +27,10 @@ public class TableViewParcela {
 	private SimpleStringProperty dataparcela;
 	private SimpleStringProperty datapagamento;
 	
+	private SimpleListProperty<Button> buttons;
+	
+	private Calendar calendar = Calendar.getInstance();
+	
 	public TableViewParcela(Long codigo, Cliente cliente, Acordo acordo, Integer parcela, Double valor, Date dataparcela,
 			Date datapagamento) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -29,11 +40,23 @@ public class TableViewParcela {
 		this.acordo = new SimpleStringProperty(acordo.getValor()+"");
 		this.parcela = new SimpleIntegerProperty(parcela);
 		this.valor = new SimpleDoubleProperty(valor);
-		Calendar calendar = Calendar.getInstance();
+		
 		calendar.setTime(dataparcela);
 		this.dataparcela = new SimpleStringProperty(sdf.format(calendar.getTime()));
 		calendar.setTime(datapagamento);
 		this.datapagamento = new SimpleStringProperty(sdf.format(calendar.getTime()));
+		
+		ObservableList<Button> bts = FXCollections.observableArrayList();
+		Button pagar = new Button("Pagar");
+		bts.add(pagar);
+		
+		bts.get(0).setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println(getCliente());
+			}
+		});
 	}
 
 	public Long getCodigo() {
@@ -68,31 +91,38 @@ public class TableViewParcela {
 		this.parcela = parcela;
 	}
 
-	public SimpleDoubleProperty getValor() {
-		return valor;
+	public Double getValor() {
+		return valor.get();
 	}
 
 	public void setValor(SimpleDoubleProperty valor) {
 		this.valor = valor;
 	}
 
-	public SimpleStringProperty getDataparcela() {
-		return dataparcela;
+	public String getDataparcela() {
+		return dataparcela.get();
 	}
 
 	public void setDataparcela(SimpleStringProperty dataparcela) {
 		this.dataparcela = dataparcela;
 	}
 
-	public SimpleStringProperty getDatapagamento() {
-		return datapagamento;
+	public String getDatapagamento() {
+		return datapagamento.get();
 	}
 
 	public void setDatapagamento(SimpleStringProperty datapagamento) {
 		this.datapagamento = datapagamento;
 	}
-	
-	
+
+	public ObservableList<Button> getButtons() {
+		return buttons.get();
+	}
+
+	public void setButtons(ObservableList<Button> buttons) {
+		this.buttons = new SimpleListProperty<Button>(buttons);
+	}
+
 	
 	
 
